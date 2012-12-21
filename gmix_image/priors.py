97129c7f1e69d17c4e38a4f81fc5cd1bfd382523
Erin Sheldon <erin.sheldon@gmail.com>
2012-12-21 18:24:06 -0500

    added variosu priors

---
+++ gmix_image/priors.py
@@ -0,0 +1,216 @@
+from numpy import sqrt, cos, sin, exp, pi, zeros,  \
+        random, where, array
+import math
+
+class GPrior:
+    """
+    This is in g1,g2 space
+
+    2D
+    Prob = A cos(|g| pi/2) exp( - [ 2 |g| / B / (1 + |g|^D) ]^C )
+    d/dE(  A cos(sqrt(E^2+q^2) pi/2) exp( - ( 2 sqrt(E^2 + q^2) / B / (1 + sqrt(E^2 + q^2)^D) )^C ) )
+
+    For 1D prob, you need to multiply by 2*pi*|g|
+    """
+    def __init__(self, A=12.25, B=0.03, C=0.45, D=13.):
+        # A actually depends on norm when doing full thing
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+
+        self.maxval = self(0., 0.)
+
+    def __call__(self, g1, g2):
+        """
+        Get the prior for the input g1,g2 value(s)
+
+        This is the 2 dimensional prior.  If you want to just
+        generate the |g| values, use prior1d
+        """
+        g = sqrt(g1**2 + g2**2)
+        return self.prior_gabs(g)
+
+    def dbyg1(self, g1, g2, h=1.e-6):
+        """
+        Derivative with respect to g1 at the input g1,g2 location
+
+        Uses central difference and a small enough step size
+        to use just two points
+        """
+        ff = self(g1+h/2, g2)
+        fb = self(g1-h/2, g2)
+
+        return (ff - fb)/h
+
+    def dbyg2(self, g1, g2, h=1.e-6):
+        """
+        Derivative with respect to g2 at the input g1,g2 location
+
+        Uses central difference and a small enough step size
+        to use just two points
+        """
+        ff = self(g1, g2+h/2)
+        fb = self(g1, g2-h/2)
+        return (ff - fb)/h
+
+
+    def prior_gabs(self, g):
+        """
+        Get the 2d prior for the input |g| value(s)
+        """
+        g = array(g, ndmin=1, copy=False)
+        prior = zeros(g.size)
+
+        w,=where(g < 1)
+        if w.size > 0:
+            prior[w] = self.A * cos(g[w]*pi/2)*exp( - ( 2*g[w] / self.B / (1 + g[w]**self.D) )**self.C )
+        return prior
+
+    def prior_gabs_scalar(self, g):
+        return self.A * math.cos(g*pi/2)*math.exp( - ( 2*g / self.B / (1 + g**self.D) )**self.C )
+
+    def sample(self, nrand, as_shear=False):
+        """
+        Get random g1,g2 values
+
+        parameters
+        ----------
+        nrand: int
+            Number to generate
+        as_shear: bool, optional
+            If True, get a list of Shear objects
+        """
+        g1 = zeros(nrand)
+        g2 = zeros(nrand)
+
+        ngood=0
+        nleft=nrand
+        while ngood < nrand:
+
+            # generate total g**2 in [0,1)
+            grand2 = random.random(nleft)
+            grand = sqrt(grand2)
+            # now uniform angles
+            rangle = random.random(nleft)*2*pi
+
+            # now get cartesion locations in g1,g2 plane
+            g1rand = grand*cos(rangle)
+            g2rand = grand*sin(rangle)
+
+            # now finally the height from [0,maxval)
+            h = self.maxval*random.random(nleft)
+
+            pvals = self(g1rand, g2rand)
+
+            w,=where(h < pvals)
+            if w.size > 0:
+                g1[ngood:ngood+w.size] = g1rand[w]
+                g2[ngood:ngood+w.size] = g2rand[w]
+                ngood += w.size
+                nleft -= w.size
+
+        if as_shear:
+            from lensing.shear import Shear
+            shlist=[]
+            for g1i,g2i in zip(g1,g2):
+                shlist.append(Shear(g1=g1i,g2=g2i))
+            return shlist
+        else:
+            return g1, g2
+
+
+    def prior1d(self, g):
+        """
+        Get the 1d prior for an input |g| value(s).
+
+        To generate 2-d g1,g2, use prior()
+        """
+        return 2*pi*g*self.prior_gabs(g)
+
+    def sample1d(self, nrand):
+        """
+        Get random |g| from the 1d distribution
+
+        parameters
+        ----------
+        nrand: int
+            Number to generate
+        """
+
+        if not hasattr(self,'maxval1d'):
+            self.set_maxval1d()
+
+        g = zeros(nrand)
+
+        ngood=0
+        nleft=nrand
+        while ngood < nrand:
+
+            # generate total g**2 in [0,1)
+            grand = random.random(nleft)
+
+            # now finally the height from [0,maxval)
+            h = self.maxval1d*random.random(nleft)
+
+            pvals = self.prior1d(grand)
+
+            w,=where(h < pvals)
+            if w.size > 0:
+                g[ngood:ngood+w.size] = grand[w]
+                ngood += w.size
+                nleft -= w.size
+   
+        return g
+
+    def set_maxval1d(self):
+        import scipy.optimize
+        
+        (minvalx, fval, iterations, fcalls, warnflag) \
+                = scipy.optimize.fmin(self.prior1dneg, 0.1, full_output=True, 
+                                      disp=False)
+        if warnflag != 0:
+            raise ValueError("failed to find min: warnflag %d" % warnflag)
+        self.maxval1d = -fval
+
+    def prior1dneg(self, g, *args):
+        """
+        So we can use the minimizer
+        """
+        return -self.prior1d(g)
+
+class CenPrior:
+    def __init__(self, cen, sigma):
+        self.cen=cen
+        self.sigma=sigma
+        self.sigma2=[s**2 for s in sigma]
+
+    def lnprob(self, pos):
+        lnprob0 = -0.5*(self.cen[0]-pos[0])**2/self.sigma2[0]
+        lnprob1 = -0.5*(self.cen[1]-pos[1])**2/self.sigma2[1]
+        return lnprob0 + lnprob1
+
+
+
+
+def test_shear_mean():
+    gp=GPrior(A= 12.25,
+              B= 0.2,
+              C= 1.05,
+              D= 13.)
+    n=100000
+    e1=zeros(n)
+    g1true,g2true=gp.sample(n)
+    shear=Shear(g1=0.04,g2=0.0)
+
+    g1meas=zeros(n)
+    for i in xrange(n):
+        s=Shear(g1=g1true[i],g2=g2true[i])
+        ssum = s + shear
+
+        g1meas[i] = ssum.g1
+
+    g1mean=g1meas.mean()
+    g1err=g1meas.std()/sqrt(n)
+    print 'shear: %.16g +/- %.16g' % (g1mean,g1err)
+
